c952067f17f90d18b4e458923ea1f9876018cbf9
Jeremy Bunting <qbunt@users.noreply.github.com>
2022-09-13 23:12:00 -0400

    first pass on structure, getting site env config set up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import {
+		PUBLIC_SITE_TITLE,
+		PUBLIC_SITE_URL,
+		PUBLIC_SITE_DESCRIPTION,
+		PUBLIC_SITE_OG_IMAGE,
+		PUBLIC_SITE_TWITTER_HANDLE
+	} from '$env/static/public'
+
+	export const prerender = true
+</script>
+
+<svelte:head>
+	<title>{PUBLIC_SITE_TITLE}</title>
+	<link rel="canonical" href={PUBLIC_SITE_URL} />
+	<link rel="alternate" type="application/rss+xml" href={PUBLIC_SITE_URL + '/rss.xml'} />
+	<meta property="og:url" content={PUBLIC_SITE_URL} />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={PUBLIC_SITE_TITLE} />
+	<meta name="Description" content={PUBLIC_SITE_DESCRIPTION} />
+	<meta property="og:description" content={PUBLIC_SITE_DESCRIPTION} />
+	<meta property="og:image" content={`${PUBLIC_SITE_URL}/images/${PUBLIC_SITE_OG_IMAGE}`} />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:creator" content={'@' + PUBLIC_SITE_TWITTER_HANDLE} />
+	<meta name="twitter:title" content={PUBLIC_SITE_TITLE} />
+	<meta name="twitter:description" content={PUBLIC_SITE_DESCRIPTION} />
+	<meta name="twitter:image" content={`${PUBLIC_SITE_URL}/images/${PUBLIC_SITE_OG_IMAGE}`} />
+</svelte:head>
+<h2>hi</h2>
+<slot />
--- svelte.config.js
+++ svelte.config.js
@@ -4,19 +4,19 @@ import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	extensions: ['.svelte', '.md'],
-	preprocess: [
-		preprocess(),
-		mdsvex({
-			extensions: ['.md']
-		})
-	],
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  extensions: ['.svelte', '.md'],
+  preprocess: [
+    preprocess(),
+    mdsvex({
+      extensions: ['.md']
+    })
+  ],
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter()
+  }
 }
 
 export default config
