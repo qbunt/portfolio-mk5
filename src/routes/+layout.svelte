3dbe5a2dc981732a3fd970879ef20234c3a1381d
Jeremy Bunting <qbunt@users.noreply.github.com>
2022-09-13 23:21:01 -0400

    configuring postcss, rehype

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '../app.postcss'
 	export const prerender = true
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,28 @@
 import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex'
+import autoprefixer from 'autoprefixer'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  extensions: ['.svelte', '.md'],
-  preprocess: [
-    preprocess(),
-    mdsvex({
-      extensions: ['.md']
-    })
-  ],
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		preprocess({
+			postcss: true
+		}),
+		mdsvex({
+			extensions: ['.md'],
+			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
+		})
+	],
 
-  kit: {
-    adapter: adapter()
-  }
+	kit: {
+		adapter: adapter()
+	}
 }
 
 export default config
